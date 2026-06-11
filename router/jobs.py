0dde466a80076f7f31bcf7390846f7371261986e
Khidirov Ali <alihidirov285@gmail.com>
2026-06-11 14:48:24 +0500

    leartn about request body and responces

--- router/jobs.py
+++ router/jobs.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, Field, SecretStr, EmailStr, ConfigDict, field_va
 from typing import Optional, Annotated
 from .users import User
 from pydantic.alias_generators import to_camel
+from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
@@ -18,13 +19,42 @@ class BaseSchema(BaseModel):
     )
 
 
-@router.get("/name")
-async def jobs_name():
-    return {
-        "job_count": 2,
-        1: "Frontend",
-        2: "Backend",
-    }
+
+
+
+
+
+#---------------------------------------------------------Dars_5------------------------------------------------------------------
+
+
+@router.post("/test-manual")
+async def manual_response():
+    return JSONResponse(
+        status_code=418, # I'm a teapot (hazil status kodi)
+        content={"message": "Men kofe damlay olmayman, men choynakman!"}
+    )
+
+
+class UserOut(BaseModel):
+    username: str
+    email: str
+
+
+@router.post("/test-filter", response_model=UserOut,status_code=201)
+async def test_filter(user:UserOut):
+    # bu yerda aa ni kiritamiz lekin fast api uniauto qirqib tashlaydi
+    raw_data = user.model_dump()
+    raw_data["aaa"] = "aaaaa"
+    print(raw_data)
+
+    # Biz hammasini qaytaramiz (return raw_data)
+    # Lekin response_model=UserOut bo'lgani uchun...
+    return raw_data
+
+
+
+
+#---------------------------------------------------------Dars_4------------------------------------------------------------------
 
 
 class Job(BaseSchema):
@@ -39,24 +69,12 @@ async def create_job(job:Job):
     return {"Muvaffaqiyatli yaratildi"}
 
 
-@router.get("/jobs/")
-async def read_jobs(
-        category:str = Query("Backend", pattern="^Back$", alias="h!@#$%^&ufu", description='aaaaaaaaaaaa', title='aaaaaaaaaaaaaaaaaaaaaaaaaa'),
-        limit: int=10
-):
-    return {
-        "category":category,
-        "limit":limit
-    }
-
-
 class Skill(BaseSchema):
     id: PositiveInt
     name: MinStr
 
 
 class Developer(BaseSchema):
-    model_config = ConfigDict(extra='forbid')
 
     id: PositiveInt
     full_name: MinStr = Field(..., max_length=20, description="Ismingizni kiriting")
@@ -66,15 +84,15 @@ class Developer(BaseSchema):
     @classmethod
     def name_must_be_alpha(cls, v:str):
         if not v.isalpha():
-            return ValueError("Ismda faqat xarflar bo'lisi shart")
+            raise ValueError("Ismda faqat xarflar bo'lisi shart")
         return v.title()
 
 
-@router.post("/dev/create")
+@router.post("/dev/create", response_model=Developer)
 async def dev_create(developer: Developer):
-    return {"message": " Muvaffaqiyatli yaratildi",
-            "Developer": developer
-            }
+    dev_data = developer.model_dump()
+
+    return dev_data
 
 
 class Freelancer(BaseSchema):
@@ -86,3 +104,25 @@ class Freelancer(BaseSchema):
 freelancer = Freelancer(email="alihidiorh285@gmail.com", hourly_rate=25, api_key="super-secret-token-123")
 print(freelancer.model_dump_json())
 print(freelancer.api_key.get_secret_value())
+#---------------------------------------------------------Dars_3------------------------------------------------------------------
+
+@router.get("/jobs/")
+async def read_jobs(
+        category:str = Query("Backend", pattern="^Back$", alias="h!@#$%^&ufu", description='aaaaaaaaaaaa', title='aaaaaaaaaaaaaaaaaaaaaaaaaa'),
+        limit: int=10
+):
+    return {
+        "category":category,
+        "limit":limit
+    }
+
+
+
+#---------------------------------------------------------Dars_2------------------------------------------------------------------
+@router.get("/name")
+async def jobs_name():
+    return {
+        "job_count": 2,
+        1: "Frontend",
+        2: "Backend",
+    }
