16947e2c66309ed5b6bd14a9d897d71bc55b7128
Khidirov Ali <alihidirov285@gmail.com>
2026-06-19 07:18:08 +0500

    db changed to postgresql+async

--- database.py
+++ database.py
@@ -1,10 +1,25 @@
-from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
+import os
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+load_dotenv()
 
-sqlite_url = "sqlite:///./learn.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(sqlite_url, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=True)
 
 
 class Base(DeclarativeBase):
     pass
+
+async_session_maker  = async_sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+async def get_async_session():
+    async with async_session_maker() as session:
+        yield session
