6358f3f7d22c699781ad81387833c2c4f8e9cbf5
Khidirov Ali <alihidirov285@gmail.com>
2026-06-20 14:23:52 +0500

    learnt write CRUD with DB

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from router import api_router, NegativeSalaryError, my_handler
 from fastapi.responses import RedirectResponse
 from database import Base, engine
 import models
 
-async def init_models():
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Dastur ishga tushganda (startup) jadvallarni yaratadi
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-
+    yield
 
 app = FastAPI(
     title="ALI API",
     description="Junior python backend developerning ilk FAST API loyihasi",
-    version="0.0.1"
+    version="0.0.1",
+    lifespan=lifespan
 )
 
 @app.get("/", include_in_schema=False)
@@ -21,32 +27,3 @@ async def redirect_to_docs():
 
 app.add_exception_handler(NegativeSalaryError, my_handler)
 app.include_router(api_router, prefix="/api/v1")
-
-
-
-@app.options("/kopaytma/{son1}/{son2}", tags=["math"])
-def kvadrat(son1:int, son2:int):
-    s = son1*son2
-
-    return {'Javob':f"{son1}*{son2}={s}"}
-
-
-@app.get("/health", tags=["Health"])
-def health():
-    return {"status":"active", "DB": 'ok', "message":"Loyiha juda zo'r ishlab turibdi"}
-
-
-@app.get("/info", tags=["Info"])
-def info():
-    return {
-        "Version": "0.0.1",
-        "engine": "uvicorn (ASGI)",
-        "Framework": "Fast API",
-        "Ega":"sceptik Ali va uning Phd ustozi Ai studio",
-    }
-
-@app.post("/items/{item_id}",tags=['items'])
-def items(item_id:int):
-    return {"Item":item_id}
-
-
--- models/jobs.py
+++ models/jobs.py
@@ -7,7 +7,16 @@ class Job(Base):
     __tablename__ = "jobs"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    title: Mapped[str] = mapped_column(String(20), nullable=False)
+    title: Mapped[str] = mapped_column(String(50), nullable=False)
     description: Mapped[str | None] = mapped_column(String(250))
     salary: Mapped[float] = mapped_column(default=0)
     created_at: Mapped[datetime] = mapped_column(server_default=text("CURRENT_TIMESTAMP"))
+
+    def __repr__(self):
+        return f"""
+            "id": {self.id},
+            "title": {self.title},
+            "description": {self.description},
+            "salary": {self.salary},
+            "created_at": {self.created_at}
+        """
