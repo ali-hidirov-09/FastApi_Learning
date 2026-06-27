96e9a209adae26483223d20f1a849d30aa2a8269
Khidirov Ali <alihidirov285@gmail.com>
2026-06-27 19:02:04 +0500

    learnt about JWT auth refresh and access tokens and wrote absolutely secure code

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,3 @@
 from .jobs import Job
-from .users import Author, Book
+from .users import Author, Book, User
+from .refresh_tokens import RefreshToken
--- router/__init__.py
+++ router/__init__.py
@@ -1,4 +1,4 @@
-from router import jobs,users
+from router import jobs,users, auth
 from fastapi import APIRouter
 from .jobs import my_handler, NegativeSalaryError
 api_router = APIRouter()
@@ -6,3 +6,4 @@ api_router = APIRouter()
 
 api_router.include_router(jobs.router, prefix="/jobs", tags=["Jobs"])
 api_router.include_router(users.router, prefix="/users", tags=['Users'])
+api_router.include_router(auth.router, prefix="/auth", tags=['Auth'])
